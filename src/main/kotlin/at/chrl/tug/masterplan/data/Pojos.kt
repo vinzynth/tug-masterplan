6bef6c9b3147ccfed59d279140432e0aca6c48e3
Christian Leopold <christian.leopold@bravestone.com>
2016-02-24 17:57:31 +0100

    Added Dataclasses

---
+++ src/main/kotlin/at/chrl/tug/masterplan/data/Pojos.kt
@@ -0,0 +1,46 @@
+package at.chrl.tug.masterplan.data
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+/**
+ * Created by ChRL on 24.02.16.
+ * Project: masterplan
+ * Package: at.chrl.tug.masterplan.data
+ * <p>
+ * <br>
+ */
+@Entity
+data class Lecture(
+        @Id
+        @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
+        val id: Long,
+        val lectureNumber: Int,
+        val title: String,
+        val hours: Double,
+        val ects: Double,
+        val type: LectureType,
+        val compulsoryTypes: List<CompulsoryType>
+){}
+
+enum class LectureType{
+    VO, KU, UE, VU, SE, SP, PR, LU
+}
+
+enum class CompulsoryType{
+    FREE, ELECTIVE, COMPULSORY
+}
+
+enum class Catalogue(val title: String){
+    c01("Secure and Correct Systems"),
+    c02("Computer Vision and Graphics"),
+    c03("Robotics and Computational Intelligence"),
+    c04("Signal Processing and Human Communication"),
+    c05("Communications and Mobile Computing"),
+    c06("Embedded and Automotive Systems"),
+    c07("Measurement and Control Systems"),
+    c08("Microelectronics and IC Design"),
+    s01("Supplementary catalogue"),
+    b01("Business, Law, and Management")
+}
---
+++ src/main/kotlin/at/chrl/tug/masterplan/data/Repositories.kt
@@ -0,0 +1,15 @@
+package at.chrl.tug.masterplan.data
+
+import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.stereotype.Repository
+
+/**
+ * Created by ChRL on 24.02.16.
+ * Project: masterplan
+ * Package: at.chrl.tug.masterplan.data
+ * <p>
+ * <br>
+ */
+@Repository
+interface LectureRepository : PagingAndSortingRepository<Lecture, Long> {
+}
