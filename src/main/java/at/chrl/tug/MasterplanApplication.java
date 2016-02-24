4546043c614a9b04bd09404bb3071333945739b3
Christian Leopold <christian.leopold@bravestone.com>
2016-02-24 17:32:44 +0100

    initial commit

---
+++ src/main/java/at/chrl/tug/MasterplanApplication.java
@@ -0,0 +1,12 @@
+package at.chrl.tug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterplanApplication.class, args);
+	}
+}
